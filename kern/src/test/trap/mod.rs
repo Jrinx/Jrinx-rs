9f70fba1b2eeb43913befcdf12b5f505e2b8e804
coekjan <cn_yzr@qq.com>
2023-11-24 18:17:35 +0800

    内核单元测试中使用用户示例库中的程序，而非从内核拷贝代码到用户态

--- kern/src/test/trap/mod.rs
+++ kern/src/test/trap/mod.rs
@@ -1,3 +1,14 @@
+use elf::{
+    abi::{PF_R, PF_W, PF_X},
+    endian::AnyEndian,
+    ElfBytes,
+};
+use jrinx_hal::{Cache, Hal, Vm};
+use jrinx_loader::ElfLoader;
+use jrinx_paging::{GenericPagePerm, GenericPageTable, PagePerm};
+use jrinx_phys_frame::PhysFrame;
+use jrinx_vmm::KERN_PAGE_TABLE;
+
 pub(super) mod breakpoint {
     use jrinx_hal::Hal;
     use jrinx_testdef::testdef;
@@ -14,87 +25,35 @@ pub(super) mod breakpoint {
 }
 
 pub(super) mod page_fault {
-    use cfg_if::cfg_if;
     use jrinx_addr::VirtAddr;
-    use jrinx_hal::{Cache, Hal, Vm};
-    use jrinx_paging::{GenericPagePerm, GenericPageTable, PagePerm};
-    use jrinx_phys_frame::PhysFrame;
+    use jrinx_paging::{GenericPagePerm, PagePerm};
     use jrinx_testdef::testdef;
     use jrinx_trap::{arch::Context, GenericContext, TrapReason};
-    use jrinx_vmm::KERN_PAGE_TABLE;
-
-    cfg_if! {
-        if #[cfg(any(target_arch = "riscv32", target_arch = "riscv64"))] {
-            macro_rules! code_read_zero {
-                ($addr:expr) => {
-                    {
-                        unsafe {
-                            core::ptr::copy_nonoverlapping(
-                                check_addr_protection as *const u8,
-                                $addr.as_usize() as *mut u8,
-                                4
-                            );
-                        }
-                        hal!().cache().sync_all();
-                        #[naked]
-                        unsafe extern "C" fn check_addr_protection() -> ! {
-                            core::arch::asm!("lw zero, 0(zero)", options(noreturn));
-                        }
-                    }
-                };
-            }
-
-            macro_rules! code_write_zero {
-                ($addr:expr) => {
-                    {
-                        unsafe {
-                            core::ptr::copy_nonoverlapping(
-                                check_addr_protection as *const u8,
-                                $addr.as_usize() as *mut u8,
-                                4
-                            );
-                        }
-                        hal!().cache().sync_all();
-                        #[naked]
-                        unsafe extern "C" fn check_addr_protection() -> ! {
-                            core::arch::asm!("sw zero, 0(zero)", options(noreturn));
-                        }
-                    }
-                };
-            }
-        }
-    }
-
-    const USER_TEXT: usize = 0x10000;
 
     #[testdef]
     fn test() {
+        let nullptr_reader = jrinx_uprog::find("test/nullptr-reader").unwrap();
+        let nullptr_writer = jrinx_uprog::find("test/nullptr-writer").unwrap();
+
+        let nullptr_reader_entry = nullptr_reader.ehdr.e_entry as usize;
+        let nullptr_writer_entry = nullptr_writer.ehdr.e_entry as usize;
+
         let mut ctx = Context::default();
-        ctx.user_setup(USER_TEXT, 0);
+
+        ctx.user_setup(0, 0);
         ctx.disable_int();
         ctx.run();
         assert_eq!(
             ctx.trap_reason(),
             TrapReason::PageFault {
-                addr: VirtAddr::new(USER_TEXT),
+                addr: VirtAddr::new(0),
                 perm: PagePerm::X,
             }
         );
 
-        let frame = PhysFrame::alloc().unwrap();
-        let addr = frame.addr();
-        KERN_PAGE_TABLE
-            .write()
-            .map(
-                VirtAddr::new(USER_TEXT),
-                frame,
-                PagePerm::U | PagePerm::R | PagePerm::X,
-            )
-            .unwrap();
-
-        hal!().vm().sync_all();
-
-        code_read_zero!(addr.to_virt());
+        super::load_elf(nullptr_reader);
+        ctx.user_setup(nullptr_reader_entry, 0);
+        ctx.disable_int();
         ctx.run();
         assert_eq!(
             ctx.trap_reason(),
@@ -104,7 +63,9 @@ pub(super) mod page_fault {
             }
         );
 
-        code_write_zero!(addr.to_virt());
+        super::load_elf(nullptr_writer);
+        ctx.user_setup(nullptr_writer_entry, 0);
+        ctx.disable_int();
         ctx.run();
         assert_eq!(
             ctx.trap_reason(),
@@ -117,65 +78,65 @@ pub(super) mod page_fault {
 }
 
 pub(super) mod syscall {
-    use jrinx_addr::VirtAddr;
-    use jrinx_hal::{Cache, Hal, Vm};
-    use jrinx_paging::{GenericPagePerm, GenericPageTable, PagePerm};
-    use jrinx_phys_frame::PhysFrame;
     use jrinx_testdef::testdef;
     use jrinx_trap::{arch::Context, GenericContext, TrapReason};
-    use jrinx_vmm::KERN_PAGE_TABLE;
-
-    #[cfg(any(target_arch = "riscv32", target_arch = "riscv64"))]
-    macro_rules! code_syscall_with_num {
-        ($addr:expr, $num:literal) => {
-            {
-                unsafe {
-                    core::ptr::copy_nonoverlapping(
-                        syscall_with_num as *const u8,
-                        $addr.as_usize() as *mut u8,
-                        8
-                    );
-                }
-                hal!().cache().sync_all();
-                #[naked]
-                unsafe extern "C" fn syscall_with_num() -> ! {
-                    core::arch::asm!("ori a7, zero, {}", "ecall", const $num, options(noreturn));
-                }
-            }
-        };
-    }
-
-    const USER_TEXT: usize = 0x10000;
 
     #[testdef]
     fn test() {
+        let system_caller = jrinx_uprog::find("test/system-caller").unwrap();
+        let system_caller_entry = system_caller.ehdr.e_entry as usize;
+
+        super::load_elf(system_caller);
+
         let mut ctx = Context::default();
 
-        let frame = PhysFrame::alloc().unwrap();
-        let addr = frame.addr();
-        KERN_PAGE_TABLE
-            .write()
-            .map(
-                VirtAddr::new(USER_TEXT),
-                frame,
-                PagePerm::U | PagePerm::R | PagePerm::X,
-            )
-            .unwrap();
-
-        hal!().vm().sync_all();
-
-        code_syscall_with_num!(addr.to_virt(), 32);
-        ctx.user_setup(USER_TEXT, 0);
+        ctx.user_setup(system_caller_entry, 0);
         ctx.disable_int();
         ctx.run();
         assert_eq!(ctx.trap_reason(), TrapReason::SystemCall);
-        assert_eq!(ctx.syscall_num(), 32);
-
-        code_syscall_with_num!(addr.to_virt(), 64);
-        ctx.user_setup(USER_TEXT, 0);
-        ctx.disable_int();
-        ctx.run();
-        assert_eq!(ctx.trap_reason(), TrapReason::SystemCall);
-        assert_eq!(ctx.syscall_num(), 64);
+        assert_eq!(ctx.syscall_num(), 0xC0DE);
     }
+}
+
+fn load_elf(elf: ElfBytes<'_, AnyEndian>) {
+    ElfLoader::new(elf)
+        .load(|elf, phdr, vaddr, offst, len| {
+            let mut perm = PagePerm::V | PagePerm::U;
+            if phdr.p_flags & PF_R != 0 {
+                perm |= PagePerm::R;
+            }
+            if phdr.p_flags & PF_W != 0 {
+                perm |= PagePerm::W;
+            }
+            if phdr.p_flags & PF_X != 0 {
+                perm |= PagePerm::X;
+            }
+            let mut page_table = KERN_PAGE_TABLE.write();
+
+            let paddr = if let Ok((phys_frame, old_perm)) = page_table.lookup(vaddr) {
+                let paddr = phys_frame.addr();
+                if !old_perm.contains(perm) {
+                    page_table.map(vaddr, phys_frame, perm | old_perm).unwrap();
+                }
+                paddr
+            } else {
+                let phys_frame = PhysFrame::alloc().unwrap();
+                let paddr = phys_frame.addr();
+                page_table.map(vaddr, phys_frame, perm).unwrap();
+                paddr
+            };
+            if len != 0 {
+                unsafe {
+                    core::ptr::copy_nonoverlapping(
+                        elf.segment_data(phdr).unwrap().as_ptr(),
+                        (paddr.to_virt().as_usize() + offst) as *mut u8,
+                        len,
+                    );
+                }
+            }
+            Ok(())
+        })
+        .unwrap();
+    hal!().cache().sync_all();
+    hal!().vm().sync_all();
 }
