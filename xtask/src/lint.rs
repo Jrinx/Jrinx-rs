4b68229f0b27a76367d1f365c6f562faf82c34cd
coekjan <cn_yzr@qq.com>
2023-11-24 18:40:00 +0800

    lint 时按需创建假的用户程序包

--- xtask/src/lint.rs
+++ xtask/src/lint.rs
@@ -1,4 +1,4 @@
-use std::process::ExitStatus;
+use std::{env, fs, path::Path, process::ExitStatus};
 
 use crate::{
     envs,
@@ -8,6 +8,13 @@ use crate::{
 
 pub fn run(arg: &MakeArg) -> Option<ExitStatus> {
     envs::setup(arg);
+
+    let uprog_path = env::var_os("UPROG_PATH").unwrap();
+    let uprog_path = Path::new(uprog_path.to_str().unwrap());
+
+    if !Path::exists(uprog_path) {
+        fs::write(uprog_path, "MOCK UPROG BINARY").unwrap();
+    }
 
     let mut cmd = Cargo::new("clippy");
 
