5078a492003bdf000a51d19aa367b6c45f43bdb9
coekjan <cn_yzr@qq.com>
2023-12-06 21:35:16 +0800

    允许从其他的 allocator 中分配 `PhysFrame`

--- kern/modules/phys-frame/src/lib.rs
+++ kern/modules/phys-frame/src/lib.rs
@@ -13,8 +13,9 @@ use core::{
 };
 
 #[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
-pub struct PhysFrame {
+pub struct PhysFrame<A: Allocator = Global> {
     addr: PhysAddr,
+    alloc: A,
 }
 
 #[repr(C, align(4096))]
@@ -22,10 +23,10 @@ struct PhysFrameMemory([u8; jrinx_config::PAGE_SIZE]);
 
 const PHYS_FRAME_MEMORY_LAYOUT: Layout = Layout::new::<PhysFrameMemory>();
 
-impl Drop for PhysFrame {
+impl<A: Allocator> Drop for PhysFrame<A> {
     fn drop(&mut self) {
         unsafe {
-            Global.deallocate(
+            self.alloc.deallocate(
                 NonNull::new(self.addr().to_virt().as_usize() as *mut u8).unwrap(),
                 PHYS_FRAME_MEMORY_LAYOUT,
             );
@@ -35,13 +36,23 @@ impl Drop for PhysFrame {
 
 impl PhysFrame {
     pub fn alloc() -> Result<Arc<Self>> {
+        Self::alloc_in(Global)
+    }
+}
+
+impl<A: Allocator> PhysFrame<A> {
+    pub fn alloc_in(alloc: A) -> Result<Arc<Self>> {
         let addr: NonNull<u8> =
-            core::hint::black_box(Global.allocate_zeroed(PHYS_FRAME_MEMORY_LAYOUT))
+            core::hint::black_box(alloc.allocate_zeroed(PHYS_FRAME_MEMORY_LAYOUT))
                 .map_err(|_| InternalError::NotEnoughMem)?
                 .cast();
-        Ok(Arc::new(Self {
+
+        let frame = Self {
             addr: VirtAddr::new(addr.as_ptr() as usize).to_phys(),
-        }))
+            alloc,
+        };
+
+        Ok(Arc::new(frame))
     }
 
     pub fn addr(&self) -> PhysAddr {
