9b8a2b617e5352a6adfdb4ab46ade5e0ee4757d2
coekjan <cn_yzr@qq.com>
2024-02-07 22:13:10 +0800

    新增 `duration_as_time` helper 函数

--- apex/src/time.rs
+++ apex/src/time.rs
@@ -11,3 +11,11 @@ pub fn time_as_duration(time: ApexSystemTime) -> core::time::Duration {
         core::time::Duration::from_nanos(time as u64)
     }
 }
+
+pub fn duration_as_time(duration: core::time::Duration) -> ApexSystemTime {
+    if duration == core::time::Duration::MAX {
+        APEX_TIME_INFINITY
+    } else {
+        duration.as_nanos() as ApexSystemTime
+    }
+}
